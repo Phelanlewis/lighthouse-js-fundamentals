cf59a205defc80a8210d552dcbfb7cae9a54750b
Phelan Lewis <phelanlewis@gmail.com>
2016-11-24 18:51:57 +0000

    Added conditionals exercises & coding challanges

---
+++ go-outside.js
@@ -0,0 +1,45 @@
+//basic if else statements
+var raining = true;
+var cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+//next part with else if statements
+
+var temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+//the && || section
+
+// && operator
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+// || operator
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// ! operator
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
